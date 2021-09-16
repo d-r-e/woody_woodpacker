eac34e5f3fee13e61a8435f0260db76cf73b6ab8
David R <egoz3ntrum@gmail.com>
2021-09-16 15:52:54 +0000

    219c340e1964

--- src/main.c
+++ src/main.c
@@ -27,7 +27,11 @@ int	main(int argc, char **argv)
 	ret = 0;
 	if (argc != 2)
 		usage();
-	printf("iself? %d\n", is_elf(argv[1]));
+	if (!is_elf(argv[1]))
+	{
+		printf("File architecture not suported. x86_64 only\n");
+		exit(-1);
+	}
 	ret = read_file(argv[1]);
 	exit(ret);
 }
