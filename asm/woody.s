795a2dc7eb6d5eb2caffcfe878890615d9786002
David R <egoz3ntrum@gmail.com>
2021-09-29 15:44:39 +0000

    e3025afe8b4d

--- asm/woody.s
+++ asm/woody.s
@@ -1,21 +1,33 @@
 [BITS] 64
 segment .text
-    global _start
-_start:
-    push rax
-    push rdi
-    push rsi
-    push rdx        ;save registers
+    global _woody
+_woody:
+	pushfq
+	push rax
+	push rdi
+	push rsi
+	push rsp
+	push rdx
+	push rcx
+	push r8
+	push r9
+	push r10
     mov rax, 1      ; syscall write
     mov rdi, 1      ; fd 1
     mov rsi, .wdy    ; buffer woody
-    mov rdx, 15     ; bytes 14
+    mov rdx, 15     ; bytes 15
     syscall
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rax         ; restore regs
-    jmp 0x4003e0
+	pop r10
+	pop r9
+	pop r8
+	pop rcx
+	pop rdx
+	pop rsp
+	pop rsi
+	pop rdi
+	pop rax
+	popfq
+    jmp 0x424242
     .wdy db "....WOODY.....", 0xa, 0; 
     ; xor rdi, rdi
     ; mov rax, 60
